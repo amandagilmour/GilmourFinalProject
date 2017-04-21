c94c51cee677660e3b02f5e26ae9dd02611b75bb
Amanda Gilmour <amandagilmour@Amandas-MacBook-Air.local>
2017-04-21 12:13:08 -0400

    Initial Commit

---
+++ Closet app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Closet app
+//
+//  Created by Amanda Gilmour on 4/21/17.
+//  Copyright © 2017 Amanda Gilmour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
